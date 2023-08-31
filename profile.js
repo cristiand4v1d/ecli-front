6b849ef2bde37ea65a0859cdc7509099a3fd1dfa
cristiand4v1d <143585588+cristiand4v1d@users.noreply.github.com>
2023-08-31 16:25:00 -0500

    Add files via upload

--- profile.js
+++ profile.js
@@ -1,5 +1,5 @@
-/* const apiUrl = "http://localhost:3000" */
-const apiUrl = "https://ecli.onrender.com"
+const apiUrl = "http://localhost:3000"
+/* const apiUrl = "https://ecli.onrender.com" */
 
 document.addEventListener("DOMContentLoaded", function () {
     const profileContainer = document.getElementById("user-profile");
@@ -83,11 +83,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 <section class="block">
             <div class="block__main">
               <div class="block__header">
-                <h2>${data.nombre}</h2>
+                <h2>${data.nombre}, ${data.edad}</h2>
               </div>
               <div class="block__content">
                 <span>
-                ${data.genero}, Mellín
+                ${data.genero}, Medellín
                 </span>
               </div>
             </div>
