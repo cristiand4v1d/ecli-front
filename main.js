7aff0af3da9c8c0e5e812305e78572ba87cd5336
cristiand4v1d <143585588+cristiand4v1d@users.noreply.github.com>
2023-08-30 21:42:40 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+    const registerForm = document.getElementById("register-form");
+
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const username = document.getElementById("login-username").value;
+        const password = document.getElementById("login-password").value;
+
+        const loginData = {
+            username: username,
+            password: password
+        };
+
+        // Enviar datos al API de inicio de sesión
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    // Suponiendo que la respuesta contiene la URL del perfil del usuario
+                    console.log(data.token)
+                    localStorage.setItem('token', JSON.stringify(data.token));
+
+                    window.location.href = 'profile.html';
+                } else {
+                    console.log('Inicio de sesión fallido'); // Manejar inicio de sesión fallido
+                }
+
+
+                // Aquí puedes manejar la respuesta del API después del inicio de sesión
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    registerForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const username = document.getElementById("register-username").value;
+        const password = document.getElementById("register-password").value;
+        const nombre = document.getElementById("register-nombre").value;
+        const genero = document.getElementById("register-genero").value;
+        const edad = document.getElementById("register-edad").value;
+        const busca = document.getElementById("register-busca").value;
+
+        const registerData = {
+            nombre: nombre,
+            genero: genero,
+            edad: edad,
+            estado: "activo",
+            password: password,
+            username: username,
+            busca: busca
+        };
+
+        // Enviar datos al API de registro
+        fetch('http://localhost:3000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(registerData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Aquí puedes manejar la respuesta del API después del registro
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+});
---
+++ profile.js
@@ -0,0 +1,161 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const profileContainer = document.getElementById("user-profile");
+    const sel1 = document.getElementById("field1");
+    const logoutButton = document.getElementById("logout-button");
+    const updateButton = document.getElementById("update-button");
+    const dropdown1 = document.getElementById("dropdown1");
+    const dropdown2 = document.getElementById("dropdown2");
+    let selected
+    let intereses_selected
+    let selectedValueDropdown1
+    let selectedValueDropdown2
+
+
+    sel1.addEventListener("change", handleSelectChange);
+    let token = localStorage.getItem('token')
+    // Agregar evento de clic al botón de Cerrar Sesión
+    logoutButton.addEventListener("click", function () {
+        // Eliminar el token de localStorage
+        localStorage.removeItem("token");
+        // Redireccionar a la página de inicio de sesión
+        window.location.href = "index.html";
+    });
+
+    function handleSelectChange(event) {
+        intereses_selected = Array.from(this.selectedOptions).map(x => x.value ?? x.text)
+    }
+
+    // Agregar evento de cambio al dropdown1
+    dropdown1.addEventListener("change", function () {
+        selectedValueDropdown1 = dropdown1.value;
+        updateDropdown2Options();
+    });
+
+    // Agregar evento de cambio al dropdown2
+    dropdown2.addEventListener("change", function () {
+        selectedValueDropdown2 = dropdown2.value;
+    });
+
+    // Generar opciones para dropdown1
+    for (let i = 18; i <= 100; i++) {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        dropdown1.appendChild(option);
+    }
+
+    // Actualizar opciones para dropdown2 en función de dropdown1
+    function updateDropdown2Options() {
+        dropdown2.innerHTML = "";
+        const minAge = parseInt(dropdown1.value) + 1;
+        for (let i = minAge; i <= 100; i++) {
+            const option = document.createElement("option");
+            option.value = i;
+            option.textContent = i;
+            dropdown2.appendChild(option);
+        }
+    }
+
+    // Agregar evento de cambio al dropdown1
+    dropdown1.addEventListener("change", updateDropdown2Options);
+
+    // Generar opciones iniciales para dropdown2
+    updateDropdown2Options();
+
+
+
+    if (token) {
+        token = token.replace(/^"(.*)"$/, '$1');;
+        fetch('http://localhost:3000/profile', {
+            method: 'GET',
+            headers: {
+                'Authorization': `${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+
+                const profileInfo = `
+          <h2>Perfil de Usuario</h2>
+          <p>Nombre: ${data.nombre}</p>
+          <p>Género: ${data.genero}</p>
+          <!-- Agregar más campos según la estructura de los datos -->
+        `;
+
+                profileContainer.innerHTML = profileInfo;
+                selected = data.intereses
+
+                // Establecer valores por defecto
+                dropdown1.value = data.edad_min; // Valor por defecto para dropdown1
+                selectedValueDropdown1 = dropdown1.value;
+                dropdown2.value = data.edad_max; // Valor por defecto para dropdown2
+                selectedValueDropdown2 = dropdown2.value;
+
+                fetch('http://localhost:3000/intereses', {
+                    method: 'GET',
+                })
+                    .then(response => response.json())
+                    .then(interests => {
+
+                        //debugger
+                        /* const interestsList = interests.map(interest => `<option>${interest.nombre}</option>`).join('');
+                        InterestsList.innerHTML = interestsList; */
+                        if (selected && interests) {
+                            //debugger
+                            sel1.innerHTML =
+                                /* interests.map(t => '<option value="' + t.nombre + '">' + t.nombre + '</option>'); */
+                                interests.map(
+                                    t =>
+                                        `<option value="${t.nombre}" ${selected.includes(t.nombre) ? "selected" : ""
+                                        }>${t.nombre}</option>`
+                                ).join("");
+
+
+                            sel1.loadOptions();
+                        } else if (interests) {
+                            sel1.innerHTML =
+                                interests.map(t => '<option value="' + t.nombre + '">' + t.nombre + '</option>');
+
+                            sel1.loadOptions();
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                    });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+        /* Actualizar intereses */
+        updateButton.addEventListener("click", function () {
+            const data = {
+                edad_max: selectedValueDropdown2,
+                edad_min: selectedValueDropdown1,
+                intereses: intereses_selected
+            };
+            console.log("lemus", data)
+            fetch('http://localhost:3000/actualizar-ususario', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data); // Mensaje de respuesta del API
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        })
+
+
+    } else {
+        window.location.href = 'index.html';
+    }
+});
+
+
