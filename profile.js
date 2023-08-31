fd95eb002a58c204f3f524b645a1be9f102bd824
cristiand4v1d <143585588+cristiand4v1d@users.noreply.github.com>
2023-08-31 15:59:19 -0500

    Add files via upload

--- profile.js
+++ profile.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const updateButton = document.getElementById("update-button");
     const dropdown1 = document.getElementById("dropdown1");
     const dropdown2 = document.getElementById("dropdown2");
+    const alertContainer = document.querySelector(".alert-container");
     let selected
     let intereses_selected
     let selectedValueDropdown1
@@ -141,6 +142,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         /* Actualizar intereses */
         updateButton.addEventListener("click", function () {
+            mostrarSpinner();
             const data = {
                 edad_max: selectedValueDropdown2,
                 edad_min: selectedValueDropdown1,
@@ -157,9 +159,13 @@ document.addEventListener("DOMContentLoaded", function () {
             })
                 .then(response => response.json())
                 .then(data => {
+                    ocultarSpinner();
+                    showAlert("info", data.message);
                     console.log(data); // Mensaje de respuesta del API
                 })
                 .catch(error => {
+                    ocultarSpinner();
+                    showAlert("danger", error);
                     console.error('Error:', error);
                 });
         })
@@ -168,6 +174,40 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
         window.location.href = 'index.html';
     }
+
+    function showAlert(type, message) {
+        /* const profileInfo = `<div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+      `;
+
+        alertContainer.innerHTML = profileInfo; */
+        const alert = document.createElement("div");
+        alert.classList.add("alert", `alert-${type}`, "alert-dismissible", "fade", "show");
+        alert.setAttribute("role", "alert");
+        alert.innerHTML = `
+           ${message}
+         `;
+
+        alertContainer.appendChild(alert);
+
+        // Cierra la alerta automáticamente después de 5 segundos
+        setTimeout(() => {
+            alert.remove();
+        }, 5000);
+    }
+
+    // Mostrar el spinner cuando se realiza una solicitud al servidor
+    function mostrarSpinner() {
+        console.log("lemus")
+        document.getElementById("spinnerContainer").style.display = "flex";
+    }
+
+    // Ocultar el spinner cuando se recibe la respuesta del servidor
+    function ocultarSpinner() {
+        document.getElementById("spinnerContainer").style.display = "none";
+    }
+
 });
 
 
